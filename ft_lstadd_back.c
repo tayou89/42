f81a7d2a2f4b6b19722aef6b9bdfdb3471b3f1a8
tayou89 <ysh00718@gmail.com>
2022-11-19 23:21:38 +0900

    22/11/19 23:21

---
+++ ft_lstadd_back.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayou <tayou@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 21:50:18 by tayou             #+#    #+#             */
+/*   Updated: 2022/11/19 23:21:20 by tayou            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*copy_lst;
+
+	copy_lst = *lst;
+	if (copy_lst == NULL)
+	{
+		copy_lst = new;
+		return ;
+	}
+	while (copy_lst->next != NULL)
+		copy_lst = copy_lst->next;
+	copy_lst->next = new;
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	t_list	*lst[10];
+	t_list	*new;
+	char	*s1 = "111111";
+	char	*s2 = "222222";
+	char	*s3 = "333333";
+	char	*s4 = "new";
+	int		size;
+	int		i;
+
+	lst[0] = ft_lstnew((void *) s1);
+	lst[1] = ft_lstnew((void *) s2);
+	lst[2] = ft_lstnew((void *) s3);
+	new = ft_lstnew((void *) s4);
+	i = 0;
+	while (i < 2)
+	{
+		lst[i]->next = lst[i + 1];
+		i++;
+	}
+	size = ft_lstsize(lst[0]);
+	printf("before list_size: %d\n", size);
+	ft_lstadd_back(&lst[0], new);
+	size = ft_lstsize(lst[0]);
+	printf("after list_size: %d\n", size);
+	return (0);
+}	
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tayou <tayou@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 00:22:28 by tayou             #+#    #+#             */
-/*   Updated: 2022/11/19 16:53:02 by tayou            ###   ########.fr       */
+/*   Updated: 2022/11/19 21:18:59 by tayou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,15 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
