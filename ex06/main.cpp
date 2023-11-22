c38fc973abb2f65444100a4c51761d8626e1fd17
tayou89 <ysh00718@gmail.com>
2023-11-22 20:55:10 +0900

    add ex06

---
+++ ex06/main.cpp
@@ -0,0 +1,57 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int		DefineComplainType(std::string complain[4], std::string userInput);
+void	PrintTitle(std::string title);
+
+int	main(int argc, char *argv[])
+{
+	Harl		Harl;
+	std::string	complain[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	int			complainType;
+
+	if (argc != 2)
+		return (0);
+	complainType = DefineComplainType(complain, argv[1]);
+	switch (complainType)
+	{
+	case 0:
+		PrintTitle(complain[0]);
+		Harl.complain(complain[0]);
+		std::cout << std::endl;
+
+	case 1:
+		PrintTitle(complain[1]);
+		Harl.complain(complain[1]);
+		std::cout << std::endl;
+
+	case 2:
+		PrintTitle(complain[2]);
+		Harl.complain(complain[2]);
+		std::cout << std::endl;
+
+	case 3:
+		PrintTitle(complain[3]);
+		Harl.complain(complain[3]);
+		std::cout << std::endl;
+		break;
+
+	default:
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return (0);
+}
+
+int	DefineComplainType(std::string complain[4], std::string userInput)
+{
+	int	i = 0;
+
+	while (complain[i] != userInput)
+		i++;
+	return (i);
+}
+
+void	PrintTitle(std::string title)
+{
+	std::cout << "[ " << title << " ]" << std::endl;
+}
