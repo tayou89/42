34e9724153835f2eb85f94d2176aa4d4bf93ebdc
tayou89 <ysh00718@gmail.com>
2023-12-04 00:24:21 +0900

    add ex03

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -4,6 +4,43 @@
 
 bool	bsp(Point const a, Point const b, Point const c, Point const point)
 {
-	float	
-	float	area[3];
+	Fixed	triangleArea = getTriangleArea(a, b, c);
+	Fixed	insideArea1 = getTriangleArea(a, b, point);
+	Fixed	insideArea2 = getTriangleArea(b, c, point);
+	Fixed	insideArea3 = getTriangleArea(a, c, point);
+	Fixed	insideAreaSum = insideArea1 + insideArea2 + insideArea3;
+
+	if (insideArea1 == 0 || insideArea2 == 0 || insideArea3 == 0)
+	{
+		std::cout << "The Point is on the edge of the triangle." << std::endl;
+		return (false);
+	}
+	if (insideAreaSum == triangleArea)
+		return (true);
+	else
+		return (false);
+}
+
+Fixed	getTriangleArea(Point a, Point b, Point c)
+{
+	Fixed	negative(-1);
+
+}
+
+void	printEveryPoint(Point const a, Point const b, Point const c, Point const point)
+{
+	printPoint("Triangle point 1: ", a);
+	printPoint("Triangle point 2: ", b);
+	printPoint("Triangle point 3: ", c);
+	printPoint("Point to check: ", point);
+}
+
+void	printPoint(std::string prompt, Point point)
+{
+	Fixed	coordinateX = point.getCoordinateX();
+	Fixed	coordinateY = point.getCoordinateY();
+
+	std::cout << prompt;
+	std::cout << "( " << coordinateX.toFloat();
+	std::cout << ", " << coordinateY.toFloat() << " )";
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -9,5 +9,9 @@ int	main(void)
 	Point	trianglePoint3(0, 1.5f);
 	point	pointToCheck(3, 3);
 
+	if (bsp(trianglePoint1, trianglePoint2, trianglePoint3, pointToCheck) == true)
+		std::cout << "The point is inside of triangle." << std::endl;
+	else
+		std::cout << "The point is not inside of triangle." << std:endl;
 	return (0);
 }
