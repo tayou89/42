d93a3b7b834d158d304ab4b0248629b73ca6fe5e
tayou89 <ysh00718@gmail.com>
2024-01-09 21:25:23 +0900

    Making test code

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -8,28 +8,27 @@ template <typename T, typename Container = std::deque<T> >
 class	MutantStack : public std::stack<T, Container>
 {
 	public:
-		typedef std::stack<T, Container>				stack_class;
-		typedef stack_class::container_type				container_type;
-		typedef container_type::iterator				iterator;
-		typedef container_type::const_iterator			const_iterator;
-		typedef container_type::reverse_iterator		reverse_iterator;
-		typedef container_type::const_reverse_iterator	const_reverse_iterator;
+		typedef std::stack<T, Container>						stack_class;
+		typedef typename stack_class::container_type			container_type;
+		typedef typename container_type::iterator				iterator;
+		typedef typename container_type::const_iterator			const_iterator;
+		typedef typename container_type::reverse_iterator		reverse_iterator;
+		typedef typename container_type::const_reverse_iterator	const_reverse_iterator;
 
 		MutantStack(void) {};
 		~MutantStack(void) {};
-		MutantStack(const MutantStack &object) { *this = object };
+		MutantStack(const MutantStack &object) { *this = object; };
 		MutantStack	&operator=(const MutantStack &object);
 
-		MutantStack(const Container &container) 
-			: stack_class::stack(container) {};
-		iterator				begin(void)			{ return (this->c.begin()) };
-		const_iterator			begin(void) const	{ return (this->c.begin()) };
-		iterator				end(void) 			{ return (this->c.end()) };
-		const_iterator			end(void) const 	{ return (this->c.end()) };
-		reverse_iterator		rbegin(void)		{ return (this->c.rbegin()) };
-		const_reverse_iterator	rbegin(void) const	{ return (this->c.rbegin()) };
-		reverse_iterator		rend(void) 			{ return (this->c.rend()) };
-		const_reverse_iterator	rend(void) const 	{ return (this->c.rend()) };
+		MutantStack(const Container &container) : stack_class::stack(container) {};
+		iterator				begin(void)			{ return (this->c.begin()); };
+		const_iterator			begin(void) const	{ return (this->c.begin()); };
+		iterator				end(void) 			{ return (this->c.end()); };
+		const_iterator			end(void) const 	{ return (this->c.end()); };
+		reverse_iterator		rbegin(void)		{ return (this->c.rbegin()); };
+		const_reverse_iterator	rbegin(void) const	{ return (this->c.rbegin()); };
+		reverse_iterator		rend(void) 			{ return (this->c.rend()); };
+		const_reverse_iterator	rend(void) const 	{ return (this->c.rend()); };
 };
 
 template <typename T, typename Container>
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,8 +1,19 @@
 #include "MutantStack.hpp"
 #include <iostream>
 
+void	testMutantStack1(void);
+void	testList(void);
+
 int main()
 {
+	testMutantStack1();
+	testList();
+	return (0);
+}
+
+void	testMutantStack1(void)
+{
+	std::cout << "\n=============== MutantStack Test 1 ===============\n";
 	MutantStack<int> mstack;
 
 	mstack.push(5); 
@@ -31,5 +42,51 @@ int main()
 	}
 
 	std::stack<int> s(mstack); 
-	return 0;
+}
+
+#include <list>
+
+void	testList(void)
+{
+	std::cout << "\n=============== List Test ===============\n";
+	std::list<int> mstack;
+
+	mstack.push_back(5); 
+	mstack.push_back(17);
+
+	std::cout << mstack.back() << std::endl; 
+
+	mstack.pop_back();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push_back(3); 
+	mstack.push_back(5); 
+	mstack.push_back(737); 
+
+	std::list<int>::iterator it = mstack.begin(); 
+	std::list<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+
+	while (it != ite) 
+	{
+    	std::cout << *it << std::endl;
+		++it; 
+	}
+
+	std::stack<int, std::list<int> > s(mstack); 
+}
+
+#include <vector>
+
+void	testMutantStack2(void)
+{
+	typedef MutantStack<int, std::vector<int> >	VectorStack;	
+
+	int					array[] = { 5, 4, 2, 100, 30, 20, -6 };
+	std::vector<int>	vector(array, array + sizeof(array) / sizeof(int));
+	const VectorStack	constVectorStack(vector);
+	MutantStack<int>	mutantStack;
 }
